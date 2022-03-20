d1670fb206fd6ca382d6be771ca721665bfa1837
pisuthd <pisuth.dae@gmail.com>
2022-03-20 13:23:31 +0700

    feat: add SCC collection

---
+++ projects/4-scc/constants.js
@@ -0,0 +1,28 @@
+const ID = 4
+const NAME = "Smart Collector Card"
+const CHAIN_ID = 1
+const ASSETS = [
+  {
+    address: "0xf0e4f1fd2f2289c77fbe9aca62d4a1ee5dc9d327",
+    is1155: false,
+    useArchive: false,
+    startBlock: 13595172
+  }
+]
+
+const IMAGE_URL = "https://img.tamago.finance/luckbox/scc-logo.jpg"
+
+const DESCRIPTION = "SCC is a collector club in the NFT world. SCC Card means a membership in the club. SCC Card holders can participate in exclusive events such as NFT claims, raffles, community giveaways, and more."
+
+const ARCHIVE = {
+}
+
+module.exports = {
+  ID,
+  NAME,
+  CHAIN_ID,
+  ASSETS,
+  ARCHIVE,
+  IMAGE_URL,
+  DESCRIPTION
+}
