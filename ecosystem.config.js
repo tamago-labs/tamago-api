e09ee9c92d5b91dcd373da3d87db542a1f30232a
Moopz-Dev <pongzthor@gmail.com>
2022-03-31 11:53:25 +0700

    fix: set cronjob autorestart to false

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,7 @@
+//IMPORTANT
+// In case of maintenance or discontinuation of usage, `pm2 stop all` would termainate all pm2 jobs but they will restart again at the time specified by'cron_restart'parameter.  Use `pm2 delete all` to terminate and kill all the processes instead. 
+
+
 const fs = require("fs");
 
 const apps = [];
@@ -7,7 +11,9 @@ fs.readdirSync("./projects/").map((project, index) =>
     script: "./services/fetch-holders/index.js",
     args: String(project),
     instances: 1,
-    cron_restart: `${Math.floor(index / 10)} ${index % 10} * * *`
+    cron_restart: `${Math.floor(index / 10)} ${index % 10} * * *`,
+    // cron_restart: '*/1 * * * *', //this restarts the job every minute and it is intended for testing-purposes and should be turned-off by default
+    autorestart: false //if set to 'true'(default value for this key) then the job would restart at every successful or errorful exit and causes infinite restart loop, the value for this key-pair must be kept as 'false'.  
   })
 );
 console.log(apps);
