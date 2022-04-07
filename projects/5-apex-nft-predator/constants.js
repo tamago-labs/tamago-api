46e3430e6eeaa7f3e48d741a9f48b132667b735d
Moopz-Dev <pongzthor@gmail.com>
2022-04-07 11:48:25 +0700

    add: constants for apex-nft-predator,cheese-badges,lucky-envelope-polygon

---
+++ projects/5-apex-nft-predator/constants.js
@@ -0,0 +1,31 @@
+const ID = 5;
+const NAME = "ApeX NFT Predator";
+const CHAIN_ID = 1;
+const ASSETS = [
+  {
+    address: "0x7Af3460d552f832fD7E2DE973c628ACeA59B0712",
+    is1155: false,
+    useArchive: true,
+    startBlock: 14287882,
+  },
+];
+
+const TOTAL_ITEMS = 4534;
+
+const IMAGE_URL = "https://lh3.googleusercontent.com/IMRDKv0dSfsVunxMyU7R5COt1sq3kZV7whIWlQVUmXiuQdJheIOKw3YReTYxnvIwX5n9FBa77P1qkdxOV59Zfa_zhCr54XJS06b2Zg=s130";
+
+const DESCRIPTION =
+  "ApeX Predator NFTs are your entry tickets for the ApeX NFT Game and the ApeX Loyalty Program. All early holders of Predator NFTs will be entitled to exclusive benefits — the longer the holding period, the more rewards you can grab.";
+
+const ARCHIVE = {};
+
+module.exports = {
+  ID,
+  NAME,
+  CHAIN_ID,
+  ASSETS,
+  ARCHIVE,
+  IMAGE_URL,
+  DESCRIPTION,
+  TOTAL_ITEMS,
+};
---
+++ projects/7-lucky-red-envelope-on-polygon/constants.js
@@ -0,0 +1,31 @@
+const ID = 7;
+const NAME = "Lucky Red Envelope on Polygon";
+const CHAIN_ID = 137;
+const ASSETS = [
+  {
+    address: "0x74ff3978db218e621bcde8114b544bb605adfa08",
+    is1155: true,
+    useArchive: true,
+    startBlock: 23670573,
+  },
+];
+
+const TOTAL_ITEMS = 3;
+
+const IMAGE_URL = "https://lh3.googleusercontent.com/xA_iN57897FxI1sMVLwNCESGisF-iwZhM85pSAray595byydjo7hHsvOIfmQuRBISMz77fbeT7-Zxriw1AMZJnpIW_ODDufJ0lAhicc=s130";
+
+const DESCRIPTION =
+  "The first value-backed NFT collection that was invented by Tamago Finance team, each represents specific amount of US currency from $1, $10, $100 and can be redeemed via app-v2.tamago.finance";
+
+const ARCHIVE = {};
+
+module.exports = {
+  ID,
+  NAME,
+  CHAIN_ID,
+  ASSETS,
+  ARCHIVE,
+  IMAGE_URL,
+  DESCRIPTION,
+  TOTAL_ITEMS,
+};
