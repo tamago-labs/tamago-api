5076d078c7a58eb7ef67bb967434baddba431727
Moopz-Dev <pongzthor@gmail.com>
2022-04-19 17:47:49 +0700

    perf : adjusts cron job query interval to 30 min

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,9 @@
 
 const fs = require("fs");
 
+const cron_minute = (int) => int % 2 === 0 ? 0 : 30
+const cron_hour = (int) => Math.floor(int / 2)
+
 const apps = [];
 fs.readdirSync("./projects/").map((project, index) =>
   apps.push({
@@ -11,7 +14,7 @@ fs.readdirSync("./projects/").map((project, index) =>
     script: "./services/fetch-holders/index.js",
     args: String(project),
     instances: 1,
-    cron_restart: `${Math.floor(index / 10)} ${index % 10} * * *`,
+    cron_restart: `${cron_minute(index)} ${cron_hour(index)} * * *`,
     // cron_restart: '*/1 * * * *', //this restarts the job every minute and it is intended for testing-purposes and should be turned-off by default
     autorestart: false //if set to 'true'(default value for this key) then the job would restart at every successful or errorful exit and causes infinite restart loop, the value for this key-pair must be kept as 'false'.  
   })
